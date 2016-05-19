d903367264e1841355bb7c58d4cb0596077fec8c
PremankuS <sukulankur@gmail.com>
2016-05-19 23:40:52 +0200

    just a test

---
+++ First Course_Last Assignment.py
@@ -0,0 +1,21 @@
+largest = None
+smallest = None
+
+while True:
+    number = raw_input("Enter a number or enter 'done' to stop the script: ")
+    if number == "done":
+        break
+    try:
+        number = float(number)
+        if smallest is None:
+            smallest = number
+        elif number < smallest:
+            smallest = number
+        if number > largest:
+            largest = number
+    except:
+        print "Please enter only numbers! You can also enter 'done' to stop the script"
+        continue
+print "the smallest and largest are:"
+print smallest
+print largest
