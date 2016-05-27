8282b154ff3b70e54af806e9265f9e1e00ba3475
PremankurS <sukulankur@gmail.com>
2016-05-27 13:37:02 +0200

    Day6

--- Homeworks_Related/Aufgabe 6.5.py
+++ Homeworks_Related/Aufgabe 6.5.py
@@ -1,10 +1,11 @@
-text = "X-DSPAM-Confidence:      0.8475"
+text = "X-DSPAM-Confidence:           0.8475"
 start = text.find("0")
 end = text.find("5")
 print start
 print end
 string = text[start:(end+1)]
 print string
+print type(string)
 float = float(string)
 print float
 print type(float)
